25d7830556a87c8d294e59264dfa89653cc3ee62
Yu-Heng-Juan <jimmy7511685@gmail.com>
2022-04-26 23:59:22 +0800

    add autostart function

--- TicketBot.py
+++ TicketBot.py
@@ -9,31 +9,35 @@ import requests
 from bs4 import BeautifulSoup
 import time
 
-start = time.time()
-driver = webdriver.Chrome()
-driver.get('https://kktix.com/events/hellochiayi0522/registrations/new')
+while(1):
+    if time.ctime() != "Tue Apr 26 23:55:50 2022":
+        continue
+    start = time.time()
+    driver = webdriver.Chrome()
+    driver.get('https://kktix.com/events/hellochiayi0522/registrations/new')
 
-# 加載事先存好的 cookie
-try:
-    cookies = pickle.load(open('./db_cookie.pickle', 'rb'))
-    for cookie in cookies:
-        driver.add_cookie(cookie)
-except:
-    print('Cookie file not exist!')
+    # 加載事先存好的 cookie
+    try:
+        cookies = pickle.load(open('./db_cookie.pickle', 'rb'))
+        for cookie in cookies:
+            driver.add_cookie(cookie)
+    except:
+        print('Cookie file not exist!')
 
-# 網頁刷新
-driver.refresh()
+    # 網頁刷新
+    driver.refresh()
 
-button = driver.find_element_by_xpath("//div[@id='ticket_450819']/div[1]/span[@class='ticket-quantity ng-scope']/button[2]")
-button.click()
-button.click()
+    button = driver.find_element_by_xpath("//div[@id='ticket_450819']/div[1]/span[@class='ticket-quantity ng-scope']/button[2]")
+    button.click()
+    button.click()
 
-checkbox = driver.find_element_by_xpath("//*[@id='person_agree_terms']")
-checkbox.click()
+    checkbox = driver.find_element_by_xpath("//*[@id='person_agree_terms']")
+    checkbox.click()
 
-# 尚未售票時 button 會被隱藏起來，F12 就可以看到 button element，他只是被 disable 起來
-nextstep_btn = driver.find_element_by_xpath("//*[@id='registrationsNewApp']/div/div[5]/div[4]/button") 
-nextstep_btn.click()
+    # 尚未售票時 button 會被隱藏起來，F12 就可以看到 button element，他只是被 disable 起來
+    nextstep_btn = driver.find_element_by_xpath("//*[@id='registrationsNewApp']/div/div[5]/div[4]/button") 
+    nextstep_btn.click()
 
-end = time.time()
-print (end - start)
+    end = time.time()
+    print (end - start)
+    break
