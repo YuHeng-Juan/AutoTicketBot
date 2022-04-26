2b4c0db14041024356c0d1c805118d074747ac9b
Yu-Heng-Juan <jimmy7511685@gmail.com>
2022-04-26 23:41:45 +0800

    fix TicketBot button's bug and add a new file for setting cookie

--- TicketBot.py
+++ TicketBot.py
@@ -11,30 +11,28 @@ import time
 
 start = time.time()
 driver = webdriver.Chrome()
-driver.get('https://kktix.com')
-
-# 第一次先把 cookie 存到 pickle檔
-# input("Press any button.")
-# cookies = driver.get_cookies()
-# with open('db_cookie.pickle','wb') as f:
-#     pickle.dump(cookies,f)
+driver.get('https://kktix.com/events/hellochiayi0522/registrations/new')
 
 # 加載事先存好的 cookie
-cookies = pickle.load(open('./db_cookie.pickle', 'rb'))
-for cookie in cookies:
-    driver.add_cookie(cookie)
+try:
+    cookies = pickle.load(open('./db_cookie.pickle', 'rb'))
+    for cookie in cookies:
+        driver.add_cookie(cookie)
+except:
+    print('Cookie file not exist!')
 
 # 網頁刷新
 driver.refresh()
 
-button = driver.find_element_by_xpath("//div[@id='ticket_449177']/div[1]/span[@class='ticket-quantity ng-scope']/button[2]")
+button = driver.find_element_by_xpath("//div[@id='ticket_450819']/div[1]/span[@class='ticket-quantity ng-scope']/button[2]")
 button.click()
 button.click()
 
 checkbox = driver.find_element_by_xpath("//*[@id='person_agree_terms']")
 checkbox.click()
 
-nextstep_btn = driver.find_element_by_xpath("//*[@id='registrationsNewApp']/div/div[5]/div[5]/button")
+# 尚未售票時 button 會被隱藏起來，F12 就可以看到 button element，他只是被 disable 起來
+nextstep_btn = driver.find_element_by_xpath("//*[@id='registrationsNewApp']/div/div[5]/div[4]/button") 
 nextstep_btn.click()
 
 end = time.time()
